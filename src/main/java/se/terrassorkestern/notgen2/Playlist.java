520bb4195ede6a9cd08571d1597e4f7f592b2bb2
martin <martin@hagelin.nu>
2018-12-06 08:13:32 +0100

    Fix for date handling

--- src/main/java/se/terrassorkestern/notgen2/Playlist.java
+++ src/main/java/se/terrassorkestern/notgen2/Playlist.java
@@ -1,12 +1,9 @@
 package se.terrassorkestern.notgen2;
 
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.util.Date;
-
-import static javax.persistence.TemporalType.DATE;
+import java.time.LocalDate;
 
 @Data
 @Entity
@@ -20,15 +17,7 @@ public class Playlist {
     private String name;
     @Column(name="kommentar")
     private String comment;
-
-/*
     @Column(name="datum")
     private LocalDate date;
-*/
-
-    @Temporal(DATE)
-    @DateTimeFormat(pattern="YYYY-mm-dd")
-    @Column(name="datum")
-    private Date date;
 
 }
