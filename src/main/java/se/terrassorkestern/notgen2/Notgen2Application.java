805d36a5590a1dc53bfd4321e5e016c30fbef2f2
martin <martin@hagelin.nu>
2019-01-24 12:47:03 +0100

    Nu då?

--- src/main/java/se/terrassorkestern/notgen2/Notgen2Application.java
+++ src/main/java/se/terrassorkestern/notgen2/Notgen2Application.java
@@ -10,12 +10,4 @@ public class Notgen2Application {
     SpringApplication.run(Notgen2Application.class, args);
   }
 
-  /*
-   * @Configuration public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter
-   * {
-   * 
-   * @Override protected void configure(HttpSecurity http) throws Exception {
-   * http.authorizeRequests().anyRequest().permitAll() .and().csrf().disable(); } }
-   */
-  
 }
