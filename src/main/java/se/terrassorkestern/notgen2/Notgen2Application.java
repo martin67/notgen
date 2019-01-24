8260f3a9f8178d5e93d602490f38dfcead4f7c43
martin <martin@hagelin.nu>
2019-01-24 12:41:57 +0100

    Maybe fixes the Order problem...

--- src/main/java/se/terrassorkestern/notgen2/Notgen2Application.java
+++ src/main/java/se/terrassorkestern/notgen2/Notgen2Application.java
@@ -2,9 +2,6 @@ package se.terrassorkestern.notgen2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class Notgen2Application {
