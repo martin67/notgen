9c5c155b91fb6ff8f0ce01bd577d92d77291388d
Martin Hagelin <martin@hagelin.nu>
2020-10-15 22:16:50 +0200

    Test refactoring

--- src/test/java/se/terrassorkestern/notgen2/controller/HomeControllerTest.java
+++ src/test/java/se/terrassorkestern/notgen2/controller/HomeControllerTest.java
@@ -1,24 +1,33 @@
-package se.terrassorkestern.notgen2;
+package se.terrassorkestern.notgen2.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import se.terrassorkestern.notgen2.HomeController;
+import se.terrassorkestern.notgen2.user.UserRepository;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(HomeController.class)
+@DisplayName("Home controller")
 class HomeControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private UserRepository userRepository;
+
     @Test
-    //@WithUserDetails
     void testHomePage() throws Exception {
-        mvc.perform(get("/"))
+        mvc.perform(get("/")
+                .contentType(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"))
                 .andExpect(content().string(containsString("Välkommen till")));
--- src/test/java/se/terrassorkestern/notgen2/controller/PlaylistControllerTest.java
+++ src/test/java/se/terrassorkestern/notgen2/controller/PlaylistControllerTest.java
@@ -33,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @WebMvcTest(PlaylistController.class)
+@DisplayName("Playlist controller")
 class PlaylistControllerTest {
 
     @Autowired
