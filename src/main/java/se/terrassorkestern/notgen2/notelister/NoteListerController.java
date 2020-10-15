7bdd35cb87cb7684182c01ce679130a85d7c154e
Martin Hagelin <martin@hagelin.nu>
2020-10-15 23:19:17 +0200

    Test refactoring

--- src/main/java/se/terrassorkestern/notgen2/notelister/NoteListerController.java
+++ src/main/java/se/terrassorkestern/notgen2/notelister/NoteListerController.java
@@ -1,30 +1,29 @@
 package se.terrassorkestern.notgen2.notelister;
 
-import lombok.AllArgsConstructor;
-import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
-@AllArgsConstructor
 @RequestMapping("/noteLister")
 public class NoteListerController {
 
-    private final @NonNull NoteListerService noteListerService;
+    private final NoteListerService noteListerService;
 
+    public NoteListerController(NoteListerService noteListerService) {
+        this.noteListerService = noteListerService;
+    }
 
     @GetMapping(value = {"", "/"})
-    public String noteLister(Model model) {
+    public String noteLister() {
         log.info("Nu är vi i noteLister(Model model)");
         return "noteLister";
     }
 
     @GetMapping("/generate")
-    public String noteListerGenerate(Model model) {
+    public String noteListerGenerate() {
         log.info("Nu är vi i noteListerGenerate");
         noteListerService.createList();
         return "noteLister";
