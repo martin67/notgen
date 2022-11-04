20dce6d9f6e81a31cfef0894e8224ce409c90021
Martin Hagelin <martin@hagelin.nu>
2022-11-04 22:23:17 +0100

    Test development in branch. Also update some dependencies

--- src/main/java/se/terrassorkestern/notgen/controller/HomeController.java
+++ src/main/java/se/terrassorkestern/notgen/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "about";
+    }
 }
