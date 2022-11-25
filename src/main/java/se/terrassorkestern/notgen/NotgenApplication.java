9df83870de153ee25eae5c98b9920002df69fe87
Martin Hagelin <martin@hagelin.nu>
2022-11-25 17:49:38 +0100

    sonar test

--- src/main/java/se/terrassorkestern/notgen/NotgenApplication.java
+++ src/main/java/se/terrassorkestern/notgen/NotgenApplication.java
@@ -3,7 +3,6 @@ package se.terrassorkestern.notgen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class NotgenApplication {
 
