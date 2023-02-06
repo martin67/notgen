158a600b293b102d4522614329a3e909a0bce08a
Martin Hagelin <martin@hagelin.nu>
2023-02-06 12:14:51 +0100

    notgen-21 Databasexport fungerar inte

--- src/main/java/se/terrassorkestern/notgen/service/AdminService.java
+++ src/main/java/se/terrassorkestern/notgen/service/AdminService.java
@@ -22,9 +22,9 @@ import java.util.zip.ZipOutputStream;
 @Service
 public class AdminService {
 
-    public static final List<String> tables = List.of("song", "band", "score", "instrument", "user_",
+    public static final List<String> tables = List.of("band", "score", "instrument", "user_",
             "setting", "playlist", "role", "privilege",
-            "score_instrument", "setting_instrument", "score_playlist", "role_privilege", "user_role");
+            "score_instrument", "setting_instrument", "score_playlist", "role_privilege");
     private final DataSource dataSource;
 
     public AdminService(DataSource dataSource) {
