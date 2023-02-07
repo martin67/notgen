0e2535336c68b50b9dd430466a17eed4a4752f84
Martin Hagelin <martin@hagelin.nu>
2023-02-07 16:27:54 +0100

    Fix for score "Express"

--- src/main/java/se/terrassorkestern/notgen/service/UserRepositoryUserDetailsService.java
+++ src/main/java/se/terrassorkestern/notgen/service/UserRepositoryUserDetailsService.java
@@ -1,5 +1,6 @@
 package se.terrassorkestern.notgen.service;
 
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,9 @@ public class UserRepositoryUserDetailsService implements UserDetailsService {
     public UserPrincipal loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User '" + username + "' not found"));
+        if (!user.isEnabled()) {
+            throw new DisabledException("Your account is disabled. Please contact the band admin.");
+        }
         return UserPrincipal.create(user);
     }
 
