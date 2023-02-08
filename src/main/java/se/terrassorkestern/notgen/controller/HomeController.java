33f05bae4004471a7fc34273b839cb148c5a68f5
Martin Hagelin <martin@hagelin.nu>
2023-02-09 00:20:16 +0100

    Find git info

--- src/main/java/se/terrassorkestern/notgen/controller/HomeController.java
+++ src/main/java/se/terrassorkestern/notgen/controller/HomeController.java
@@ -1,13 +1,23 @@
 package se.terrassorkestern.notgen.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
 
+    @Value("${git.commit.id.abbrev}")
+    private String GitCommit;
+    @Value("${git.commit.time}")
+    private String GitTime;
+
     @GetMapping("/")
     public String home() {
+        log.info("git.commit.id.abbrev: {}", GitCommit);
+        log.info("git.commit.time: {}", GitTime);
         return "home";
     }
 
