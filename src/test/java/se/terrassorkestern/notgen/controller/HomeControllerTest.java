a0cc63bb7f75104ec5701364fdfddfd1e856ab02
Martin Hagelin <martin@hagelin.nu>
2023-02-25 16:58:22 +0100

    Home controller testing

--- src/test/java/se/terrassorkestern/notgen/controller/HomeControllerTest.java
+++ src/test/java/se/terrassorkestern/notgen/controller/HomeControllerTest.java
@@ -1,37 +1,67 @@
 package se.terrassorkestern.notgen.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import se.terrassorkestern.notgen.configuration.SecurityConfig;
 import se.terrassorkestern.notgen.repository.UserRepository;
+import se.terrassorkestern.notgen.user.CustomOAuth2UserService;
+import se.terrassorkestern.notgen.user.CustomOidcUserService;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest
+@Import(HomeController.class)
+@ContextConfiguration(classes = SecurityConfig.class)
 @DisplayName("Home controller")
 class HomeControllerTest {
 
     @Autowired
+    private WebApplicationContext context;
+
     private MockMvc mvc;
+
     @MockBean
     private UserRepository userRepository;
+    @MockBean
+    private CustomOAuth2UserService customOAuth2UserService;
+    @MockBean
+    private CustomOidcUserService customOidcUserService;
 
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @Test
     void testHomePage() throws Exception {
         mvc.perform(get("/")
-                .contentType(MediaType.TEXT_HTML))
+                        .contentType(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"));
-//                .andExpect(content().string(containsString("Välkommen till")));
+    }
+
+    @Test
+    void testAboutPage() throws Exception {
+        mvc.perform(get("/about")
+                        .contentType(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name("about"));
     }
 
 }
