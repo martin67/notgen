500f7d0624482e23385eee0eb15d93979278275d
Martin Hagelin <martin@hagelin.nu>
2023-05-15 10:05:46 +0200

    Typo fix

--- src/test/java/se/terrassorkestern/notgen/controller/ScoreControllerTest.java
+++ src/test/java/se/terrassorkestern/notgen/controller/ScoreControllerTest.java
@@ -1,6 +1,5 @@
 package se.terrassorkestern.notgen.controller;
 
-import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
