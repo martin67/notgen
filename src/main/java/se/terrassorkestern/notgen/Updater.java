7270f4e4cc32eb7a65d2d925f0dc3cfe2fb21096
Martin Hagelin <martin@hagelin.nu>
2024-03-20 17:33:50 +0100

    Ta bort uppdatering

--- src/main/java/se/terrassorkestern/notgen/Updater.java
+++ src/main/java/se/terrassorkestern/notgen/Updater.java
@@ -5,10 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import se.terrassorkestern.notgen.model.Arrangement;
-import se.terrassorkestern.notgen.model.Score;
 import se.terrassorkestern.notgen.model.ScoreType;
-import se.terrassorkestern.notgen.repository.ScoreRepository;
 
 import java.util.List;
 
@@ -16,11 +13,8 @@ import java.util.List;
 @Component
 @Transactional
 public class Updater {
-    final
-    ScoreRepository scoreRepository;
 
-    public Updater(ScoreRepository scoreRepository) {
-        this.scoreRepository = scoreRepository;
+    public Updater() {
     }
 
     @EventListener
@@ -28,18 +22,6 @@ public class Updater {
         log.info("********* Updater start");
         var st = List.of(ScoreType.values());
         log.info("Score types: {}", st.size());
-        log.info("Syncing arranger and publisher");
-        for (Score score : scoreRepository.findAll()) {
-            if (score.getArrangements().isEmpty()) {
-                log.info("Score {} har inga arr", score);
-            } else  {
-                for (Arrangement arrangement : score.getArrangements()) {
-                    arrangement.setArranger(score.getArranger());
-                    arrangement.setPublisher(score.getPublisher());
-                }
-                scoreRepository.save(score);
-            }
-        }
         log.info("********* Updater end");
     }
 }
